05578c76ab92c1eba752af5775603d835dba3e0b
Your Nameemail <shareef.99997@gmail.com>
2024-05-19 22:35:14 +0300

    whatsapp

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -8,6 +8,15 @@ import Footer from './Footer/Footer';
 function Home() {
   return (
     <div className="Home">
+      <a className="whatsappa" href="https://api.whatsapp.com/send?phone=966566664530" target="_blank" rel="noopener noreferrer">
+      <nav className="whatsapp-button">
+       
+          <div>
+          <img src={require('../Assets/Icons/whatsapp.png')} alt="WhatsApp" />
+          </div>
+          
+        <h3>تواصل معنا</h3>
+     </nav></a> 
       <Header />
       <About />
       <Services />
