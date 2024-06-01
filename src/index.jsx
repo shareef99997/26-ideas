2985d9a90319d9358329f95261ad46b8a8dc5e5f
Your Nameemail <shareef.99997@gmail.com>
2024-06-01 22:20:17 +0300

    try react snap

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<React.StrictMode><App /></React.StrictMode>, rootElement);
+} else {
+  ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement);
+}
+
 
